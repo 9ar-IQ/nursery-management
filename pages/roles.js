a7b83c66b17db9f7525646bd0cc33c702f7052e5
9ar-IQ <mohumedhamad@gmail.com>
2025-10-30 16:05:24 +0300

    Create roles.js

---
+++ pages/roles.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Roles() {
+  return (
+    <div>
+      <h1>Roles & Permissions</h1>
+      {/* Add roles and permissions management here */}
+    </div>
+  );
+}
