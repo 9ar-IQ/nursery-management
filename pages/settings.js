e1942168b4787909aa04177feb0beb8d6f007f7d
9ar-IQ <mohumedhamad@gmail.com>
2025-10-30 16:06:17 +0300

    Create settings.js

---
+++ pages/settings.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Settings() {
+  return (
+    <div>
+      <h1>System Settings</h1>
+      {/* Add currency, timezone, and category settings UI here */}
+    </div>
+  );
+}
